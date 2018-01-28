437dedf10875f3c7b80fc9414cfc8601970e12bd
Squallium <squallium3@gmail.com>
2018-01-28 17:00:40 +0100

    Data context helper example

--- my-meteor-blog/client/templates/home.js
+++ my-meteor-blog/client/templates/home.js
@@ -1,23 +1,30 @@
 Template.home.created = function () {
-  console.log('Created the home template');
+    console.log('Created the home template');
 };
 
 Template.home.rendered = function () {
-  console.log('Rendered the home template');
+    console.log('Rendered the home template');
 };
 
 Template.home.destroyed = function () {
-  console.log('Destroyed the home template');
+    console.log('Destroyed the home template');
 };
 
 Template.home.helpers({
     exampleHelper: function () {
         return 'This text came from a helper with some <strong>HTML</strong>.';
-    }
-});
+    },
 
-Template.home.helpers({
     exampleHelperSafe: function () {
         return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong> safe.');
-    }
+    },
+
+    dataContextHelper: function () {
+        return {
+            someText: 'This data was set using a helper of the parent template.',
+            someNested: {
+                text: 'That comes from "someNested.text"'
+            }
+        };
+    },
 });
