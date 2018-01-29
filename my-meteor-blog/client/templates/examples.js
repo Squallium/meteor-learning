14684afb89706cfe485942a4360ed542398bd812
Squallium <squallium3@gmail.com>
2018-01-29 23:40:12 +0100

    Events

--- my-meteor-blog/client/templates/examples.js
+++ my-meteor-blog/client/templates/examples.js
@@ -4,6 +4,14 @@ Template.contextExample.rendered = function () {
 
 Template.contextExample.helpers({
     logContext: function () {
-      console.log('Context Log Helper', this)
+        console.log('Context Log Helper', this)
+
+        return Session.get('randomNumber');
     },
+});
+
+Template.contextExample.events({
+    'click button': function (e, template) {
+        Session.set('randomNumber', Math.random(0, 99));
+    }
 });
