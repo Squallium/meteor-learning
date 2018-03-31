733b5fad801ec4e411aea5b9922ec6ad6c3b6e2c
Squallium <squallium3@gmail.com>
2018-03-31 17:36:39 +0200

    vamos a añadir un router

--- my-meteor-blog/client/templates/home.js
+++ my-meteor-blog/client/templates/home.js
@@ -4,7 +4,6 @@ Template.home.created = function () {
 
 Template.home.rendered = function () {
     console.log('Rendered the home template');
-    this.find('p').innerHTML = 'We just replaced that text!';
 };
 
 Template.home.destroyed = function () {
@@ -12,24 +11,14 @@ Template.home.destroyed = function () {
 };
 
 Template.home.helpers({
-    exampleHelper: function () {
-        return 'This text came from a helper with some <strong>HTML</strong>.';
-    },
-
-    exampleHelperSafe: function () {
-        return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong> safe.');
-    },
-
-    dataContextHelper: function () {
-        return {
-            someText: 'This data was set using a helper of the parent template.',
-            someNested: {
-                text: 'That comes from "someNested.text"'
-            }
-        };
-    },
-
     postsList: function(){
         return Posts.find({}, {sort: {timeCreated: -1}});
     },
+});
+
+Template.home.events({
+    'click button.lazyload': function(e, template){
+        var currentLimit = Session.get('lazyloadLimit');
+        Session.set('lazyloadLimit', currentLimit + 2);
+    }
 });
